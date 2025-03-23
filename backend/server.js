21d314f7b46aa2c241ba6ae8f63eee4aabb81b0f
Halleluyah Adesuyi <adesuyihalleluyah@gmail.com>
2025-03-23 23:55:31 +0100

    chore: specify server port in use

--- backend/server.js
+++ backend/server.js
@@ -5,5 +5,5 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Server is ready'));
 
-// listen to port
+// listen to port 5000
 app.listen(port, () => console.log('Server started on port ${port}'));
