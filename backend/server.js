e69eadb7be73a0f6e6e87f36c665976f3c875964
Halleluyah Adesuyi <adesuyihalleluyah@gmail.com>
2025-03-25 22:22:08 +0100

    feat: initialize Express server with user routes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from 'express';
-const port = 5000;
+import dotenv from 'dotenv';
+dotenv.config();
+const port = process.env.PORT || 5000;
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 
+app.use('/api/users', userRoutes);
+
 app.get('/', (req, res) => res.send('Server is ready'));
 
-// listen to port 5000
-app.listen(port, () => console.log('Server started on port ${port}'));
+app.listen(port, () => console.log(`Server started on port ${port}`));
