2db6c6f14beaf81fbef6a53665a3fa790a19ec49
Halleluyah Adesuyi <adesuyihalleluyah@gmail.com>
2025-04-01 23:35:07 +0100

    chore: add comments to improve code readability

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config();    // Load environment variables from .env file
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 const port = process.env.PORT || 5000;
 import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 
+// Mount user-related routes under /api/users
 app.use('/api/users', userRoutes);
 
+// Define a simple root route for testing the server
 app.get('/', (req, res) => res.send('Server is ready'));
 
-app.use(notFound);
-app.use(errorHandler);
+app.use(notFound);    // Middleware to handle 404 errors for undefined routes
+app.use(errorHandler);    //Middleware to handle errors and return formatted responses
 
+// Start the server and listen on the specified port
 app.listen(port, () => console.log(`Server started on port ${port}`));
