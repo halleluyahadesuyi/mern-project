a6ad1c74d004750970033d8ce6b52d5f7e4adec2
Halleluyah Adesuyi <adesuyihalleluyah@gmail.com>
2025-04-03 22:41:30 +0100

    chore: resolve MongoDB connectivity issues by ensuring proper authentication and IP whitelisting

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
-dotenv.config();    // Load environment variables from .env file
+dotenv.config(); // Load environment variables from .env file
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
 const port = process.env.PORT || 5000;
 import userRoutes from './routes/userRoutes.js';
+
+connectDB();
 
 const app = express();
 
@@ -13,8 +16,8 @@ app.use('/api/users', userRoutes);
 // Define a simple root route for testing the server
 app.get('/', (req, res) => res.send('Server is ready'));
 
-app.use(notFound);    // Middleware to handle 404 errors for undefined routes
-app.use(errorHandler);    //Middleware to handle errors and return formatted responses
+app.use(notFound); // Middleware to handle 404 errors for undefined routes
+app.use(errorHandler); //Middleware to handle errors and return formatted responses
 
 // Start the server and listen on the specified port
 app.listen(port, () => console.log(`Server started on port ${port}`));
