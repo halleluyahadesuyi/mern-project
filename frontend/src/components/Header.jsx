4992660e6edc620bc3dd6429d33c8846e2c4f8db
Halleluyah Adesuyi <adesuyihalleluyah@gmail.com>
2025-05-28 02:25:29 +0100

    feat(auth): implement login screen with form and routing
    - Added LoginScreen with email/password fields and submit handler
    - Wrapped form in reusable FormContainer
    - Linked to register route and integrated into router
    - Auto-formatted Header.jsx via Prettier

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,21 +5,21 @@ import { LinkContainer } from 'react-router-bootstrap';
 const Header = () => {
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <LinkContainer to="/">
+          <LinkContainer to='/'>
             <Navbar.Brand>MERN App</Navbar.Brand>
           </LinkContainer>
 
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <LinkContainer to="/login">
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <LinkContainer to='/login'>
                 <Nav.Link>
                   <FaSignInAlt /> Sign In
                 </Nav.Link>
               </LinkContainer>
-              <LinkContainer to="/register">
+              <LinkContainer to='/register'>
                 <Nav.Link>
                   <FaSignOutAlt /> Sign Up
                 </Nav.Link>
